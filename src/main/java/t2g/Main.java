bc5570a5c64e59f3b10fc41c350d89ccc4e5db80
hako-fuusen <balloon.in.the.box@gmail.com>
2014-07-11 03:02:24 +0900

    #5 タスクトレイアイコンの表示と終了メニューの実装

--- src/main/java/t2g/Main.java
+++ src/main/java/t2g/Main.java
@@ -1,10 +1,36 @@
 package t2g;
 
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public static void main(String[] args) throws AWTException, IOException {
+		setSystemTrayIcon();
+	}
 
+	private static void setSystemTrayIcon() throws AWTException, IOException {
+		// 参考ソース http://d.hatena.ne.jp/fuzzhead/20081027/p1
+		Image image = ImageIO.read(Thread.currentThread().getContextClassLoader().getResourceAsStream("icon-tray.png"));
+
+		// それ以降は基本的にjdk8のドキュメントのサンプルから
+		MenuItem exitMenuItem = new MenuItem("終了");
+		exitMenuItem.addActionListener(e -> System.exit(0));
+
+		PopupMenu popup = new PopupMenu();
+		popup.add(exitMenuItem);
+
+		TrayIcon trayIcon = new TrayIcon(image, "Tweet2Growl", popup);
+
+		SystemTray systemTray = SystemTray.getSystemTray();
+		systemTray.add(trayIcon);
 	}
 
 }
