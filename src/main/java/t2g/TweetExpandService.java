d9cb7a475f7c1095d38c3940c25cc89ea1e23099
hako-fuusen <balloon.in.the.box@gmail.com>
2014-07-13 19:59:58 +0900

    #12　URL展開処理のリファクタリング
    ラムダ内で値変換がスマートでなかったので、データ変換用のクラスで変換を行うようにした。

--- src/main/java/t2g/TweetExpandService.java
+++ src/main/java/t2g/TweetExpandService.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 
 import twitter4j.Status;
+import twitter4j.URLEntity;
 
 public enum TweetExpandService {
 	INSTANCE;
@@ -12,11 +13,27 @@ public enum TweetExpandService {
 
 		String expandTweetText = Arrays
 				.stream(status.getURLEntities())
+				.map(ExpandURLEntity::new)
 				.reduce(text,
-						(expandingText, entity) ->  expandingText.replaceAll(Pattern.quote(entity.getURL()), entity.getExpandedURL()) ,
+						(expandingText, entity) ->  expandingText.replaceFirst(entity.tcoURL, entity.originalURL) ,
 						(expandingText1, expandingText2) -> expandingText2
 						);
 
 		return expandTweetText;
+	}
+
+	private class ExpandURLEntity {
+		/** 置換前URL(t.co形式) */
+		final String tcoURL;
+
+		/** 置換後URL */
+		final String originalURL;
+
+		public ExpandURLEntity(URLEntity entity) {
+			// String#replaceXXX を用いて文字列置換を行うため、置換前文字列をエスケープ処理が必要
+			this.tcoURL = Pattern.quote(entity.getURL());
+			this.originalURL = entity.getExpandedURL();
+		}
+
 	}
 }
