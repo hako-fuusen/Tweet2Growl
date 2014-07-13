388b27dd347c12aaf70c526a5ec28016732e3d3b
hako-fuusen <balloon.in.the.box@gmail.com>
2014-07-13 19:42:32 +0900

    #12　本文中のURL展開を実装

--- src/main/java/t2g/TweetExpandService.java
+++ src/main/java/t2g/TweetExpandService.java
@@ -1,11 +1,22 @@
 package t2g;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 import twitter4j.Status;
 
 public enum TweetExpandService {
 	INSTANCE;
 
 	public String expandTweet (String text, Status status) {
-		return text;
+
+		String expandTweetText = Arrays
+				.stream(status.getURLEntities())
+				.reduce(text,
+						(expandingText, entity) ->  expandingText.replaceAll(Pattern.quote(entity.getURL()), entity.getExpandedURL()) ,
+						(expandingText1, expandingText2) -> expandingText2
+						);
+
+		return expandTweetText;
 	}
 }
