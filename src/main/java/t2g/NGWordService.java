ec5d4c7b3bbd2e01cb3520e337c4b578074965c9
hako-fuusen <balloon.in.the.box@gmail.com>
2014-07-16 02:47:36 +0900

    #15 NGワードサービス本体を実装

--- src/main/java/t2g/NGWordService.java
+++ src/main/java/t2g/NGWordService.java
@@ -1,9 +1,32 @@
 package t2g;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum NGWordService {
 	INSTANCE;
 
+	private List<String> ngwords = new ArrayList<String>();
+
 	public boolean constainNGWord(String text) {
-		return false;
+		return ngwords.stream().anyMatch(ngword -> text.matches(ngword));
+	}
+
+	public void initialize() throws IOException, URISyntaxException {
+		// 画像ファイルロードの方式にならってファイル読み込み
+		URL url = Thread.currentThread().getContextClassLoader().getResource("ngword.txt");
+
+		Path path = Paths.get(url.toURI());
+
+		ngwords = Files.readAllLines(path).stream()
+				.map(ngwords -> ".*" + ngwords + ".*") // NGワードの確認は何度も行われるので、事前に加工しておく
+				.collect(Collectors.toList());
 	}
 }
