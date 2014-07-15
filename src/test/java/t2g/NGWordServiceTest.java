a8b352ac05408b7428755d1a1586bf52a3c1d662
hako-fuusen <balloon.in.the.box@gmail.com>
2014-07-16 02:44:26 +0900

    #15 テストケース追加

--- src/test/java/t2g/NGWordServiceTest.java
+++ src/test/java/t2g/NGWordServiceTest.java
@@ -36,4 +36,29 @@ public class NGWordServiceTest {
 		assertThat(NGWordService.INSTANCE.constainNGWord(testCase7), is(false));
 	}
 
+	@Test
+	public void escape() {
+		String testCase1 = "(笑)";
+
+		assertThat(NGWordService.INSTANCE.constainNGWord(testCase1), is(true));
+	}
+
+	@Test
+	public void regex() {
+		String testCase1 = "はい #はいじゃないが";
+
+		assertThat(NGWordService.INSTANCE.constainNGWord(testCase1), is(true));
+		
+		String testCase2 = "はい　#はいじゃないが";
+
+		assertThat(NGWordService.INSTANCE.constainNGWord(testCase2), is(true));
+		
+		String testCase3 = "この写真にカエルが映っています #気がついた人はRT";
+
+		assertThat(NGWordService.INSTANCE.constainNGWord(testCase3), is(true));
+		
+		String testCase4 = "#10人以上にRTされたら　ラーメンでも食べます";
+
+		assertThat(NGWordService.INSTANCE.constainNGWord(testCase4), is(true));
+	}
 }
